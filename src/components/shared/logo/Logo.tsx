d247ce4788bc1b564740b89c9a0d067e55253199
Dmytro Bulakhov <bulakhovda@gmail.com>
2025-05-26 16:48:18 +0300

    Update Logo.tsx

--- src/components/shared/logo/Logo.tsx
+++ src/components/shared/logo/Logo.tsx
@@ -1,25 +1,38 @@
+"use client";
+
+import { useTranslations } from "next-intl";
 import Image from "next/image";
 
-// import { companyName } from "@/constants/constants";
+import { cn } from "@/utils/cn";
 
 import { Link } from "@/i18n/navigation";
 
 interface LogoProps {
-  className: string;
-  textStyles: string;
+  className?: string;
+  textStyles?: string;
 }
 
-export default function Logo({ className, textStyles }: LogoProps) {
+const Logo = ({ className, textStyles }: LogoProps) => {
+  const t = useTranslations("Header");
+
   return (
-    <Link href="/" className="flex gap-2 xl:gap-[10px] items-center">
+    <Link href="/" className="flex items-center gap-2 xl:gap-3">
       <Image
         src="/images/icons/logo.svg"
         alt="logo"
         width="40"
         height="41"
-        className={className}
+        className={cn("h-auto w-[33px] xl:w-10", className)}
       />
-      <p className={`font-prosto uppercase ${textStyles}`}>company name</p>
+      <p
+        className={cn(
+          "w-24 font-micra text-xs uppercase leading-[1.22] xl:w-[120px] xl:text-sm",
+          textStyles
+        )}
+      >
+        {t("companyName")}
+      </p>
     </Link>
   );
-}
+};
+export default Logo;
