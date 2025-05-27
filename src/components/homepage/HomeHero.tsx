1f46ce7f327ae9a6d7806c72ec6bbdd44985b1fc
Dmytro Bulakhov <bulakhovda@gmail.com>
2025-05-27 17:17:02 +0300

    Update HomeHero.tsx

--- src/components/homepage/HomeHero.tsx
+++ src/components/homepage/HomeHero.tsx
@@ -1,12 +1,12 @@
-import { useTranslations } from "next-intl";
+import { getTranslations } from "next-intl/server";
 
 import ButtonOrLink from "../shared/button/ButtonOrLink ";
 
-const HomeHero = () => {
-  const t = useTranslations("HomePage.hero");
+const HomeHero = async () => {
+  const t = await getTranslations("homepage.hero");
 
   return (
-    <section className="relative h-dvh pt-[108px] xl:pt-[185px]">
+    <section className="relative h-dvh overflow-hidden pt-[108px] xl:pt-[185px]">
       <div className="container flex h-full max-w-[1280px] flex-col leading-[1.22]">
         <div>
           <p className="mb-5 font-micra text-xl xl:mb-7 xl:w-[251px]">
@@ -17,7 +17,7 @@ const HomeHero = () => {
           </p>
         </div>
 
-        <div className="fixed bottom-[84px] right-[30px] mt-auto flex w-[296px] flex-col gap-8 xl:relative xl:ml-auto xl:w-[475px] xl:flex-col-reverse xl:gap-7">
+        <div className="absolute bottom-[84px] right-[30px] mt-auto flex w-[296px] flex-col gap-8 xl:relative xl:ml-auto xl:w-[475px] xl:flex-col-reverse xl:gap-7">
           <h1 className="font-micra text-2xl leading-[1.22] text-light xl:text-[44px]">
             {t("title")}
           </h1>
@@ -25,8 +25,8 @@ const HomeHero = () => {
         </div>
       </div>
 
-      <div className="fixed bottom-2 right-[10px] -z-40 h-[334px] w-[340px] rounded-2xl bg-[url('/images/homepage/home-hero-bg.webp')] bg-cover xl:-right-[29px] xl:h-[98%] xl:w-[830px]" />
-      <div className="fixed -right-[112px] bottom-2 -z-30 aspect-[404/606] h-[606px] w-[404px] rounded-2xl bg-[url('/images/homepage/home-hero-person.webp')] bg-cover xl:-bottom-[17px] xl:right-[552px] xl:h-[100%] xl:w-auto" />
+      <div className="absolute bottom-2 right-[10px] -z-40 h-[334px] w-[340px] rounded-2xl bg-[url('/images/homepage/home-hero-bg.webp')] bg-cover xl:-right-[29px] xl:h-[98%] xl:w-[830px]" />
+      <div className="absolute -right-[112px] bottom-2 -z-30 aspect-[404/606] h-[606px] w-[404px] rounded-2xl bg-[url('/images/homepage/home-hero-person.webp')] bg-cover xl:-bottom-[17px] xl:right-[552px] xl:h-[100%] xl:w-auto" />
     </section>
   );
 };
