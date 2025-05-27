daedb3e8759b1f5283ce2c080ff374d3a1d1e0c1
Dmytro Bulakhov <bulakhovda@gmail.com>
2025-05-27 18:14:47 +0300

    feat: Added advantages section

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,9 +1,11 @@
+import HomeAdvantages from "@/components/homepage/HomeAdvantages";
 import HomeHero from "@/components/homepage/HomeHero";
 
 export default async function Home() {
   return (
     <>
       <HomeHero />
+      <HomeAdvantages />
     </>
   );
 }
---
+++ src/components/homepage/HomeAdvantages.tsx
@@ -0,0 +1,51 @@
+import { getMessages, getTranslations } from "next-intl/server";
+
+import InfoBox from "../shared/InfoBox";
+
+type AdvantageItem = {
+  title: string;
+  descr: string;
+};
+
+const HomeAdvantages = async () => {
+  const t = await getTranslations("homepage.advantages");
+
+  const messages = await getMessages();
+
+  const advantageList = messages.homepage.advantages
+    .advantageList as AdvantageItem[];
+
+  return (
+    <section className="pb-[188px] pt-[60px] xl:pb-[203px] xl:pt-[104px]">
+      <div className="container relative max-w-[1280px]">
+        <div className="xl:ml-[536px]">
+          <div className="mb-[60px] md:mb-12">
+            <h2 className="mb-4 font-micra text-2xl leading-[1.22] xl:text-5xl xl:leading-[1.22]">
+              {t("title")}
+            </h2>
+            <p className="font-light leading-[1.22]">{t("descr")}</p>
+          </div>
+
+          <ul className="grid gap-4 md:grid-cols-2">
+            {advantageList.map(({ title, descr }) => (
+              <li key={title}>
+                <InfoBox>
+                  <h3 className="mb-4 font-micra text-2xl leading-[1.22]">
+                    {title}
+                  </h3>
+                  <p className="text-sm font-light leading-[1.22] tracking-normal xl:text-base xl:leading-[1.22]">
+                    {descr}
+                  </p>
+                </InfoBox>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div className="hidden xl:absolute xl:bottom-0 xl:left-0 xl:-z-20 xl:block xl:h-[821px] xl:w-[547px] xl:bg-[url('/images/homepage/home-advantages-person.webp')] xl:bg-no-repeat" />
+      </div>
+    </section>
+  );
+};
+
+export default HomeAdvantages;
