6bc76c8375e9e36f006463e0ca34a5a9686f176a
Dmytro Bulakhov <bulakhovda@gmail.com>
2025-05-30 18:51:09 +0300

    styles: Added decor

--- src/components/contactsPage/ContactsHero.tsx
+++ src/components/contactsPage/ContactsHero.tsx
@@ -4,18 +4,16 @@ import Image from "next/image";
 const ContactsHero = async () => {
   const t = await getTranslations("contactsPage.hero");
 
-  // background: linear-gradient(188.96deg, #FFFFFF -9.07%, #E0EAFF 56.87%, #FFFFFF 112.06%);
-
   return (
-    <section className="h-[770px] bg-[linear-gradient(188.96deg,_#FFFFFF_-9.07%,_#E0EAFF.87%,_#FFFFFF_112.06%)] pb-[10px] pt-[124px] md:h-[528px] md:pb-0">
+    <section className="relative h-[770px] bg-[linear-gradient(188.96deg,_#FFFFFF_-9.07%,_#E0EAFF_56.87%,_#FFFFFF_112.06%)] pb-[10px] pt-[124px] md:h-[528px] md:pb-0">
       <div className="container h-full max-w-[1280px]">
         <div className="mx-auto flex h-full flex-col justify-between max-md:max-w-[400px] md:flex-row">
-          <p className="font-micra md:mt-auto md:max-w-[340px] xl:max-w-[479px] xl:text-[18px] xl:leading-[1.4]">
+          <p className="font-micra md:mb-[30px] md:mt-auto md:max-w-[340px] xl:mb-[50px] xl:max-w-[479px] xl:text-[18px] xl:leading-[1.4]">
             {t("descr")}
           </p>
 
-          <div className="flex flex-col max-md:-mx-[22px] md:-mr-[32px] md:mt-auto xl:-mr-[50px] xl:mt-0">
-            <h1 className="mb-5 font-micra text-[24px] max-md:mx-[22px] md:ml-auto md:mr-[32px] xl:mb-7 xl:mr-[50px] xl:text-[48px]">
+          <div className="relative z-[5] flex flex-col max-md:-mx-[22px] md:-mr-[32px] md:mt-auto xl:-mr-[50px] xl:mt-0">
+            <h1 className="mb-5 text-right font-micra text-[24px] max-md:mx-[22px] md:mr-[32px] xl:mb-7 xl:mr-[50px] xl:text-[48px]">
               {t("title")}
             </h1>
 
@@ -25,12 +23,16 @@ const ContactsHero = async () => {
                 fill
                 sizes="(max-width: 768px) 400px, (max-width: 1280px) 480px, 680px"
                 alt={t("imgAlt")}
-                className="rounded-2xl object-cover md:rounded-l-lg"
+                className="object-cover max-md:rounded-2xl md:rounded-l-lg xl:rounded-lg"
               />
             </div>
           </div>
         </div>
       </div>
+
+      <div className="absolute left-0 top-[146px] z-[3] h-[373px] w-[147px] bg-[url('/images/contactsPage/contacts-hero-decor-left.webp')] md:-left-[24px] md:top-[100px] md:-rotate-12" />
+      <div className="absolute right-0 top-[146px] z-[3] h-[306px] w-[139px] max-md:bg-[url('/images/contactsPage/contacts-hero-decor-right-mob.webp')] md:hidden" />
+      <div className="absolute left-[33%] top-0 z-[3] h-[237px] w-[421px] max-md:hidden md:bg-[url('/images/contactsPage/contacts-hero-decor-top-desk.webp')]" />
     </section>
   );
 };
