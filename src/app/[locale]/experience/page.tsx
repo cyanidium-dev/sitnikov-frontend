1c61226f0fb2e4ce0c763a5af048fbf8809a8f62
Dmytro Bulakhov <bulakhovda@gmail.com>
2025-05-30 13:28:40 +0300

    feat: Added careerPath section

---
+++ src/app/[locale]/experience/page.tsx
@@ -0,0 +1,15 @@
+import ExperienceCareerPath from "@/components/experiencepage/ExperienceCareerPath";
+import ExperienceExpertise from "@/components/experiencepage/ExperienceExpertise";
+import ExperienceProud from "@/components/experiencepage/ExperienceProud";
+
+const ExperiencePage = () => {
+  return (
+    <>
+      <ExperienceCareerPath />
+      <ExperienceExpertise />
+      <ExperienceProud />
+    </>
+  );
+};
+
+export default ExperiencePage;
