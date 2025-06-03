fc2ac9aa402a447c9ad1014b777df6dbaf79d6c8
Dmytro Bulakhov <bulakhovda@gmail.com>
2025-06-03 19:34:24 +0300

    feat: added hero section

--- src/app/[locale]/services/page.tsx
+++ src/app/[locale]/services/page.tsx
@@ -1,10 +1,12 @@
 import ServicesAnalytics from "@/components/servicespage/ServicesAnalytics";
 import ServicesConsulting from "@/components/servicespage/ServicesConsulting";
+import ServicesHero from "@/components/servicespage/ServicesHero";
 import ServicesPractice from "@/components/servicespage/ServicesPractice";
 
 const ServicesPage = () => {
   return (
     <>
+      <ServicesHero />
       <ServicesPractice />
       <ServicesAnalytics />
       <ServicesConsulting />
