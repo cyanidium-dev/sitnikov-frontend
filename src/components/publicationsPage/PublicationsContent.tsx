776e1c736b9b5706041350fd96a21a6a914a8439
Dmytro Bulakhov <bulakhovda@gmail.com>
2025-06-04 13:37:09 +0300

    Create PublicationsContent.tsx

---
+++ src/components/publicationsPage/PublicationsContent.tsx
@@ -0,0 +1,9 @@
+const PublicationsContent = () => {
+  return (
+    <section className="py-[200px]">
+      <div className="container max-w-[1280px]"></div>
+    </section>
+  );
+};
+
+export default PublicationsContent;
