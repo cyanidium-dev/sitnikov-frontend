0a5d06b76ffcbbbf051eac14729c75c436bf130f
Dmytro Bulakhov <bulakhovda@gmail.com>
2025-06-05 22:04:23 +0300

    Update PublicationContent.tsx

--- src/components/publicationPage/PublicationContent.tsx
+++ src/components/publicationPage/PublicationContent.tsx
@@ -1,12 +1,34 @@
 import { Block, SanityImage } from "@/lib/sanity/types/shared";
 
+import GallerySlider from "../shared/gallerySlider/GallerySlider";
+
 interface IPublicationContentProps {
+  title: string;
   content: (Block | SanityImage)[];
   gallery?: SanityImage[];
 }
 
-const PublicationContent = ({ content, gallery }: IPublicationContentProps) => {
-  return <div>PublicationContent</div>;
+const PublicationContent = ({
+  // content,
+  gallery,
+  title,
+}: IPublicationContentProps) => {
+  return (
+    <article>
+      <div className="container max-w-[1280px]">
+        <p className="py-[120px] md:py-[100px]">some text</p>
+
+        {gallery && (
+          <GallerySlider
+            wrapperClassName="mb-[46px]"
+            gallery={gallery}
+            imgAlt={title}
+            sliderId="1"
+          />
+        )}
+      </div>
+    </article>
+  );
 };
 
 export default PublicationContent;
