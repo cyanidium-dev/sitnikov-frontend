56f2f6013b631a763d656da6a7603afc682b16b6
Dmytro Bulakhov <bulakhovda@gmail.com>
2025-06-05 15:33:45 +0300

    refactor: fetches on Sanity

---
+++ src/lib/sanity/queries/queries.ts
@@ -0,0 +1,54 @@
+import { GalleryItem, PublicationItem } from "@/lib/sanity/types/queryTypes";
+
+import { client } from "../../sanity";
+
+export const gallery = async (): Promise<GalleryItem[]> => {
+  return await client.fetch<GalleryItem[]>(`
+      *[_type == "gallery"] {
+        title,
+        description,
+        images[] {
+          asset->{
+            url,
+          },
+          description
+        }
+      }
+    `);
+};
+
+export const getAllPublications = async (): Promise<PublicationItem[]> => {
+  return await client.fetch<PublicationItem[]>(`
+      *[_type == "post"] {
+        title,
+        previewImage{
+          asset->{url},
+          description
+        },
+        mainImage{
+          asset->{url},
+          description
+        },
+        mainImageMobile{
+          asset->{url},
+          description
+        },
+        "slug": slug.current,
+        description,
+        content,
+        gallery[]{
+          asset->{url},
+          description
+        }
+      }
+    `);
+};
+
+export const getAllAnnouncement = async () => {
+  return await client.fetch(`
+    *[_type == "announcement"] | order(startDate desc) {
+      title,
+      description,
+      startDate
+    }`);
+};
