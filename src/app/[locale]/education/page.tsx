c2bfe5477f93d54879133388ad5bc65943bec109
Dmytro Bulakhov <bulakhovda@gmail.com>
2025-06-06 17:54:07 +0300

    feat: Added hero section

--- src/app/[locale]/education/page.tsx
+++ src/app/[locale]/education/page.tsx
@@ -1,9 +1,11 @@
+import EducationContent from "@/components/educationPage/EducationContent";
 import EducationHero from "@/components/educationPage/EducationHero";
 
 const EducationPage = () => {
   return (
     <>
       <EducationHero />
+      <EducationContent />
     </>
   );
 };
