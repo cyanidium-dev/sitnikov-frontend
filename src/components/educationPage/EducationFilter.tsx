bb36c02a491eb9179242a62941adb1900b25d360
Dmytro Bulakhov <bulakhovda@gmail.com>
2025-06-09 16:27:16 +0300

    Create EducationFilter.tsx

---
+++ src/components/educationPage/EducationFilter.tsx
@@ -0,0 +1,39 @@
+import { Locale } from "@/types/locale";
+
+import ButtonOrLink from "../shared/button/ButtonOrLink ";
+import { CATEGORIES } from "./EducationContent";
+
+interface IEducationFilterProps {
+  onClick: (val: string) => void;
+  lang: Locale;
+  currentCourseCategory: string | null;
+}
+
+const EducationFilter = ({
+  onClick,
+  lang,
+  currentCourseCategory,
+}: IEducationFilterProps) => {
+  return (
+    <div className="mb-6 overflow-x-auto">
+      <div className="flex w-[928px] flex-row gap-4">
+        {CATEGORIES.map(category => (
+          <ButtonOrLink
+            key={category.value}
+            onClick={() => onClick(category.value)}
+            variant={
+              currentCourseCategory === category.value
+                ? "dark"
+                : "light-gradient"
+            }
+            className="whitespace-nowrap rounded-lg px-6 py-4 before:rounded-lg after:rounded-lg"
+          >
+            {category.label[lang]}
+          </ButtonOrLink>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default EducationFilter;
