b710831c9abed9b09f607cdaa65683cf3bce3519
Dmytro Bulakhov <bulakhovda@gmail.com>
2025-06-09 11:57:50 +0300

    Create EducationContent.tsx

---
+++ src/components/educationPage/EducationContent.tsx
@@ -0,0 +1,9 @@
+const EducationContent = async () => {
+  return (
+    <section>
+      <div className="container max-w-[1280px]"></div>
+    </section>
+  );
+};
+
+export default EducationContent;
