a24e1c126cb72bb102bf27b9a9c2ca72d9b642a2
Dmytro Bulakhov <bulakhovda@gmail.com>
2025-06-12 18:52:24 +0300

    styles: Added animations on Contacts Page

--- src/components/contactsPage/Contacts.tsx
+++ src/components/contactsPage/Contacts.tsx
@@ -1,7 +1,11 @@
 import { getMessages } from "next-intl/server";
 
-import { ContactItemRaw, mapContactList } from "@/utils/mapContactList";
+import { motion } from "framer-motion";
 
+import { ContactItemRaw, mapContactList } from "@/utils/mapContactList";
+import { listVariants } from "@/helpers/animation";
+
+import AnimatedWrapper from "../shared/animated/AnimatedWrapper";
 import { ContactCard } from "./ContactCard";
 
 const Contacts = async () => {
@@ -13,13 +17,18 @@ const Contacts = async () => {
   const mapped = mapContactList(contactList);
 
   return (
-    <section className="pb-[120px] pt-[110px]">
+    <section className="pt-[110px]">
       <div className="container max-w-[1280px]">
-        <ul className="grid gap-y-[100px] rounded-2xl bg-[linear-gradient(168.35deg,_#AFC8FF_-52.67%,_#FFFFFF_128.87%)] px-6 py-10 max-md:grid-cols-1 md:grid-cols-2 md:gap-x-10 xl:h-[240px] xl:grid-cols-4 xl:gap-x-[40px] xl:gap-y-0">
+        <AnimatedWrapper
+          as={motion.ul}
+          viewport={{ once: true, amount: 0.4 }}
+          animation={listVariants({ staggerChildren: 0.35 })}
+          className="grid gap-y-[100px] rounded-2xl bg-[linear-gradient(168.35deg,_#AFC8FF_-52.67%,_#FFFFFF_128.87%)] px-6 py-10 max-md:grid-cols-1 md:grid-cols-2 md:gap-x-10 xl:h-[240px] xl:grid-cols-4 xl:gap-x-[40px] xl:gap-y-0"
+        >
           {mapped.map(item => (
             <ContactCard key={item.title} {...item} />
           ))}
-        </ul>
+        </AnimatedWrapper>
       </div>
     </section>
   );
