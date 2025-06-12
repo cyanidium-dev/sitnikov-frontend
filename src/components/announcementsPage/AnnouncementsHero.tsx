43bb0c4538dd34293d00aecf363eafd2444767d1
Dmytro Bulakhov <bulakhovda@gmail.com>
2025-06-12 19:23:02 +0300

    styles: Added animations on Announcements Page

--- src/components/announcementsPage/AnnouncementsHero.tsx
+++ src/components/announcementsPage/AnnouncementsHero.tsx
@@ -1,5 +1,11 @@
 import { getTranslations } from "next-intl/server";
 
+import { motion } from "framer-motion";
+
+import { fadeInAnimation } from "@/helpers/animation";
+
+import AnimatedWrapper from "../shared/animated/AnimatedWrapper";
+
 const AnnouncementsHero = async () => {
   const t = await getTranslations("announcementsPage.hero");
 
@@ -8,15 +14,26 @@ const AnnouncementsHero = async () => {
       <div className="container -bottom-[258px] left-0 h-[274px] max-w-[1280px] bg-dark text-light max-md:absolute max-md:rounded-2xl md:h-full">
         <div className="flex h-full flex-col justify-between gap-5 pb-9 pt-12 max-md:mx-auto max-md:max-w-[400px] md:ml-auto md:w-[44%] md:justify-between md:pb-12 md:pt-[180px] xl:w-[570px]">
           <div className="flex flex-col gap-4 md:text-right xl:gap-5">
-            <h1 className="font-micra text-[22px] uppercase xl:text-[44px]">
-              {t("title")}
-            </h1>
-            <p className="font-micra text-[14px] uppercase max-xl:max-w-[231px] md:ml-auto xl:w-[302px] xl:text-[18px]">
-              {t("subtitle")}
-            </p>
+            <AnimatedWrapper animation={fadeInAnimation({ x: 50 })}>
+              <h1 className="font-micra text-[22px] uppercase xl:text-[44px]">
+                {t("title")}
+              </h1>
+            </AnimatedWrapper>
+
+            <AnimatedWrapper animation={fadeInAnimation({ x: 50, delay: 0.3 })}>
+              <p className="font-micra text-[14px] uppercase max-xl:max-w-[231px] md:ml-auto xl:w-[302px] xl:text-[18px]">
+                {t("subtitle")}
+              </p>
+            </AnimatedWrapper>
           </div>
 
-          <p className="max-w-[280px] text-[14px] font-light">{t("descr")}</p>
+          <AnimatedWrapper
+            as={motion.p}
+            animation={fadeInAnimation({ y: 50, delay: 0.5 })}
+            className="max-w-[280px] text-[14px] font-light"
+          >
+            {t("descr")}
+          </AnimatedWrapper>
 
           <div className="absolute right-0 top-0 h-[213px] w-[111px] max-md:rounded-2xl max-md:bg-[url('/images/announcementsPage/announcements-hero-decor-mob.webp')] md:hidden" />
         </div>
