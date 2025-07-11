547d761c823291d64ee2dacdb7b0c35b27f12351
Dmytro Bulakhov <bulakhovda@gmail.com>
2025-07-11 13:43:01 +0300

    feat: Added Meta for course

--- src/app/[locale]/education/[category]/[slug]/page.tsx
+++ src/app/[locale]/education/[category]/[slug]/page.tsx
@@ -3,6 +3,37 @@ import CourseHero from "@/components/coursePage/CourseHero";
 import { getCourseBySlug } from "@/lib/sanity/queries/courses";
 import { Locale } from "@/types/locale";
 
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ locale: Locale; slug: string }>;
+}) {
+  const { locale, slug } = await params;
+
+  const post = await getCourseBySlug(slug);
+
+  if (!post) return {};
+
+  const { title, description, previewImage } = post;
+
+  return {
+    title: title[locale],
+    description: description[locale],
+    openGraph: {
+      title: title[locale],
+      description: description[locale],
+      images: previewImage?.url ? [previewImage.url] : [],
+      type: "article",
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: title[locale],
+      description: description[locale],
+      images: previewImage?.url ? [previewImage?.url] : [],
+    },
+  };
+}
+
 const CoursePage = async ({
   params,
 }: {
